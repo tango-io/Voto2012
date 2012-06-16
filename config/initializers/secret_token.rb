2d209185ffb76210296085ef6d613a8239cab82f
Juan  Antonio Chavez Valencia <thenaox@MacBookTSs-MacBook.local>
2012-06-16 10:18:06 -0500

    Initialzed rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voto2012::Application.config.secret_token = '034fccb8db64dce0c66ab69621fd59f160f5f5c826b452ca44636345847fdac659cc39b89555680a2cf8150bbf92e26aa7e19daca43f19354ea169ab6fae4cdf'
